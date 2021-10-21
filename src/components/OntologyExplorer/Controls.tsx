1976ef7b935f5dad4c950d9145aafb5a6628dab0
Colin Megill <colinmegill@gmail.com>
2021-10-21 03:03:12 -0700

    outdegree for controls

--- src/components/OntologyExplorer/Controls.tsx
+++ src/components/OntologyExplorer/Controls.tsx
@@ -18,6 +18,8 @@ import {
   Label,
   Position,
   Switch,
+  RadioGroup,
+  Radio,
   InputGroup,
   Checkbox,
 } from "@blueprintjs/core";
@@ -32,7 +34,6 @@ interface IProps {
   uberon: null | IOntology;
   handleDagSearchChange: any;
   subsetToNode: any;
-  handleOutdegreeCutoffChange: any;
   resetSubset: any;
   setCompartment: any;
   handleHullChange: any;
@@ -41,6 +42,8 @@ interface IProps {
   handleHighlightAncestorChange: any;
   showTabulaSapiensDataset: boolean;
   handleShowTabulaSapiensChange: any;
+  minimumOutdegree: string;
+  handleMinOutdegreeChange: any;
 }
 
 interface IState {
@@ -51,7 +54,7 @@ class OntologyExplorer extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      isOpen: true,
+      isOpen: false,
     };
   }
 
@@ -65,7 +68,6 @@ class OntologyExplorer extends React.Component<IProps, IState> {
       outdegreeCutoffNodes,
       uberon,
       handleDagSearchChange,
-      handleOutdegreeCutoffChange,
       subsetToNode,
       resetSubset,
       setCompartment,
@@ -75,6 +77,8 @@ class OntologyExplorer extends React.Component<IProps, IState> {
       handleHighlightAncestorChange,
       showTabulaSapiensDataset,
       handleShowTabulaSapiensChange,
+      minimumOutdegree,
+      handleMinOutdegreeChange,
     } = this.props;
 
     const { isOpen } = this.state;
@@ -86,147 +90,198 @@ class OntologyExplorer extends React.Component<IProps, IState> {
           height: menubarHeight,
           // border: "1px solid lightblue",
           width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingLeft: 10,
+          paddingRight: 10,
         }}
       >
-        <div
+        <p
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingLeft: 10,
-            paddingRight: 10,
+            fontSize: 16,
+            fontWeight: 900,
+            margin: 0,
+            marginRight: 10,
+            padding: 0,
           }}
         >
-          <p
-            style={{
-              fontSize: 16,
-              fontWeight: 900,
-              margin: 0,
-              marginRight: 10,
-              padding: 0,
-            }}
-          >
-            cellxgene-ontology
-          </p>
-          <InputGroup
-            type="text"
-            leftIcon="geosearch"
-            placeholder="cell type search"
-            disabled={simulationRunning}
-            style={{
-              fontSize: 14,
-              margin: 0,
-              marginRight: 10,
-            }}
-            onChange={(e) => {
-              handleDagSearchChange(e);
-            }}
-            value={simulationRunning ? "computing layout..." : dagSearchText}
-          />
+          cellxgene-ontology
+        </p>
+        <InputGroup
+          type="text"
+          leftIcon="geosearch"
+          placeholder="cell type search"
+          disabled={simulationRunning}
+          style={{
+            fontSize: 14,
+            margin: 0,
+            marginRight: 10,
+          }}
+          onChange={(e) => {
+            handleDagSearchChange(e);
+          }}
+          value={simulationRunning ? "computing layout..." : dagSearchText}
+        />
 
-          {pinnedNode && !isSubset && (
-            <Button
-              icon="pie-chart"
-              onClick={subsetToNode}
-              style={{ marginRight: 10 }}
-            >
-              subset to {pinnedNode.id}
-            </Button>
-          )}
-          {pinnedNode && isSubset && (
-            <Button
-              icon="full-circle"
-              onClick={resetSubset}
-              style={{ marginRight: 10 }}
-            >
-              reset to whole
-            </Button>
-          )}
-
-          <Drawer
-            isOpen={isOpen}
-            size={560}
-            onClose={this.handleClose}
-            hasBackdrop={false}
-            canOutsideClickClose={true}
-            title="Graph configuration"
+        {pinnedNode && !isSubset && (
+          <Button
+            icon="pie-chart"
+            onClick={subsetToNode}
+            style={{ marginRight: 10 }}
           >
-            <div className={Classes.DRAWER_BODY}>
-              <div className={Classes.DIALOG_BODY}>
-                <p>
-                  Customizations to interface and interactivity, and
-                  modifications to the cell types and links shown.
-                </p>
-                <h2> Interactivity </h2>
-                <p>
-                  Change the behavior of interactions with the graph, such as
-                  hovering and hiding parts of the interface
-                </p>
-                <Checkbox
-                  checked={highlightAncestors}
-                  label="Highlight ancestors of hovered cell"
-                  onChange={handleHighlightAncestorChange}
-                />
-                <Checkbox
-                  checked={true}
-                  label="Show cell type force graph"
-                  onChange={() => {}}
-                  disabled
-                />
-                <Checkbox
-                  checked={true}
-                  label="Show sugiyama directed cyclic graph"
-                  onChange={() => {}}
-                  disabled
-                />
-                <h2> Graph overlays </h2>
-                <p>
-                  Show specific data on top of the graph to accomplish certain
-                  tasks
-                </p>
-                {/* <h4>Hulls</h4> */}
-                <Checkbox
-                  checked={hullsEnabled}
-                  label="Show hulls"
-                  onChange={handleHullChange}
-                />
-                <Checkbox
-                  checked={showTabulaSapiensDataset}
-                  label="Show distribution of Tabula Sapiens cell types"
-                  onChange={handleShowTabulaSapiensChange}
-                />
-                <h4>Node size</h4>
-                <h4>Force layout, radial or tree</h4>
-                <p>Show descendants, show ancestors</p>
-                <h2> Cell type filtering & subsetting </h2>
-                <p>
-                  You can subset to a contiguous sugraph by clicking any node
-                  and clicking subset. Click any cell type
-                </p>
-                <h4>Organism</h4>
-                <Checkbox
-                  checked={true}
-                  label="Remove mouse cell types"
-                  onChange={() => {}}
-                  disabled
-                />
-                <Checkbox
-                  checked={true}
-                  label="Remove fungal cell types"
-                  onChange={() => {}}
-                  disabled
-                />
-                <h4>Colors</h4>
-                Hover, click,
-                <h4>Link pruning</h4>
-                <p>{`Sometimes, links from parents to subchildren are helpful for tightening up highly related areas of the graph, in the case of x-->y-->z, this would be links between x and z. Other times, like from animal cell to thousands of descendants, this is undesireable and these nodes should be pruned. Setting this as a threshold facilitates both.`}</p>
-              </div>
+            subset to {pinnedNode.id}
+          </Button>
+        )}
+        {pinnedNode && isSubset && (
+          <Button
+            icon="full-circle"
+            onClick={resetSubset}
+            style={{ marginRight: 10 }}
+          >
+            reset to whole
+          </Button>
+        )}
+
+        <Drawer
+          isOpen={isOpen}
+          size={560}
+          onClose={this.handleClose}
+          hasBackdrop={false}
+          canOutsideClickClose={true}
+          title="Graph configuration"
+        >
+          <div className={Classes.DRAWER_BODY}>
+            <div className={Classes.DIALOG_BODY}>
+              <p>
+                Customizations to interface and interactivity. Some options have
+                hotkeys, these are shown as (k). The hotkey (k) pulls up a menu
+                of all hotkeys.
+              </p>
+              <h2> Interactivity </h2>
+              <p>
+                Change the behavior of interactions with the graph, such as
+                hovering and hiding parts of the interface
+              </p>
+              <Checkbox
+                checked={highlightAncestors}
+                label={"Highlight ancestors of hovered cell (a)"}
+                onChange={handleHighlightAncestorChange}
+              />
+              <Checkbox
+                checked={true}
+                label="Show cell type force graph"
+                onChange={() => {}}
+                disabled
+              />
+              <Checkbox
+                checked={true}
+                label="Show sugiyama directed cyclic graph"
+                onChange={() => {}}
+                disabled
+              />
+              <h2> Interpretive overlays </h2>
+              <p>
+                Show specific data on top of the graph to accomplish certain
+                tasks
+              </p>
+              {/* <h4>Hulls</h4> */}
+              <Checkbox
+                checked={hullsEnabled}
+                label="Show cell type hulls (h)"
+                onChange={handleHullChange}
+              />
+              <Checkbox
+                checked={false}
+                label="Show compartment hulls (u)"
+                onChange={() => {}}
+                disabled
+              />
+              <Checkbox
+                checked={showTabulaSapiensDataset}
+                label="Show distribution of Tabula Sapiens cell types"
+                onChange={handleShowTabulaSapiensChange}
+              />
+              <h2> Cell type filtering & subsetting </h2>
+              <p>
+                You can subset to a contiguous sugraph by clicking any node and
+                clicking subset. Click any cell type
+              </p>
+              <h4>Organism</h4>
+              <Checkbox
+                checked={true}
+                label="Remove mouse cell types"
+                onChange={() => {}}
+                disabled
+              />
+              <Checkbox
+                checked={true}
+                label="Remove fungal cell types"
+                onChange={() => {}}
+                disabled
+              />
+              <h4>Minimum outdegree / prune edges</h4>
+              <RadioGroup
+                label="Hide cell types with less than N descendants. Set to 1 to remove all terminal nodes in the tree. Set to 250 to remove all the small stuff, to get a  good look at the root of the tree."
+                onChange={(e) => {
+                  handleMinOutdegreeChange(e);
+                }}
+                selectedValue={minimumOutdegree}
+              >
+                <Radio label="0" value={"0"} />
+                <Radio label="1" value={"1"} />
+                <Radio label="3" value={"3"} />
+                <Radio label="5" value={"5"} />
+                <Radio label="10" value={"10"} />
+                <Radio label="50" value={"50"} />
+                <Radio label="250" value={"250"} />
+              </RadioGroup>
+              <Checkbox
+                checked={false}
+                label="Show aggregator nodes for min"
+                onChange={() => {}}
+                disabled
+              />
+              <h4>Maximum outdegree / prune from root</h4>
+              <RadioGroup
+                label="Hide cell types with more than N descendants. Set to 250 to remove highly connected cell types like 'electrically active cell' which has over 450 descendants. Set to 10 for lots of little disjoint graphs. 50 is a happy medium."
+                onChange={() => {}}
+                selectedValue={12345}
+                disabled
+              >
+                <Radio label="10" value={10} />
+                <Radio label="50" value={50} />
+                <Radio label="100" value={250} />
+                <Radio label="250" value={250} />
+                <Radio label="1000" value={1000} />
+                <Radio label="off" value={12345} />
+              </RadioGroup>
+              <Checkbox
+                checked={false}
+                label="Show aggregator nodes for max"
+                onChange={() => {}}
+                disabled
+              />
+              <h2>Force layout</h2>
+              <RadioGroup
+                label=""
+                onChange={() => {}}
+                selectedValue={"tree"}
+                disabled
+              >
+                <Radio label="Radial" value="radial" />
+                <Radio label="Tree" value="tree" />
+              </RadioGroup>
+              <h4>Colors</h4>
+              Hover, click,
+              <h4>Link pruning</h4>
+              <p>{`Sometimes, links from parents to subchildren are helpful for tightening up highly related areas of the graph, in the case of x-->y-->z, this would be links between x and z. Other times, like from animal cell to thousands of descendants, this is undesireable and these nodes should be pruned. Setting this as a threshold facilitates both.`}</p>
             </div>
-            <div className={Classes.DRAWER_FOOTER}>A lovely footer</div>
-          </Drawer>
-          <Button>Compartment</Button>
-          <Button icon="settings" onClick={this.handleOpen} />
-        </div>
+          </div>
+          <div className={Classes.DRAWER_FOOTER}>A lovely footer</div>
+        </Drawer>
+        <Button>Compartment (c)</Button>
+        <Button icon="settings" onClick={this.handleOpen} />
       </div>
     );
   }
@@ -237,15 +292,6 @@ class OntologyExplorer extends React.Component<IProps, IState> {
 
 export default OntologyExplorer;
 
-// <p style={{ marginLeft: 50 }}>
-// Remove nodes if outdegree greater than:
-// </p>
-// <InputGroup
-// type="text"
-// onChange={handleOutdegreeCutoffChange}
-// value={`${outdegreeCutoffNodes}`}
-// id="changeOutdegreeCutoffNodes"
-// />
 // {uberon &&
 // majorCompartments.map((compartmentID: string) => {
 //   const _compartment: any = uberon.get(compartmentID);
@@ -271,5 +317,9 @@ export default OntologyExplorer;
 // <p>Ways to make the graph cleaner</p>
 // <p>Some guidance</p>
 // <p>What you can do with this</p>
+
+{
+  /* <h4>Node size</h4> */
+}
 
 /* <h4>Naive substring subset</h4> */
