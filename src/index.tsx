acc613f384987b94db23fe7b7acac854dfcce550
Colin Megill <colinmegill@gmail.com>
2021-10-21 03:03:25 -0700

    add hotkeys

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "@blueprintjs/core/lib/css/blueprint.css";
+import { HotkeysProvider } from "@blueprintjs/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HotkeysProvider>
+      <App />
+    </HotkeysProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
