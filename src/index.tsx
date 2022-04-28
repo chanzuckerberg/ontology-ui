390735b03714ca89dae552076e657eeb0825c4f2
bkmartinjr <bruce@chanzuckerberg.com>
2022-04-27 17:17:53 -0700

    port from react-helmet to react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
+
 import App from "./App";
 import "@blueprintjs/core/lib/css/blueprint.css";
-// import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 import "@blueprintjs/select/lib/css/blueprint-select.css";
 import { HotkeysProvider } from "@blueprintjs/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HotkeysProvider>
-      <App />
-    </HotkeysProvider>
+    <HelmetProvider>
+      <HotkeysProvider>
+        <App />
+      </HotkeysProvider>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
