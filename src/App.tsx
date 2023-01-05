0894cfc7cb05db37fb32b427477f4c629ef49c0e
Colin Megill <colinmegill@gmail.com>
2023-01-05 13:16:47 -0800

    add light style

--- src/App.tsx
+++ src/App.tsx
@@ -53,10 +53,9 @@ function App({ basename }: { basename: string }) {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,400&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,400&display=swap"
             rel="stylesheet"
-            crossOrigin="true"
-          ></link>
+          />
         </Helmet>
 
         {graph && lattice && (
