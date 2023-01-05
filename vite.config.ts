abf74bdccef7d58b5708d2a0f0126fcb2f8cf955
Colin Megill <colinmegill@gmail.com>
2023-01-05 02:23:57 -0800

    clean up comments in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       fs: require.resolve("rollup-plugin-node-builtins"),
-      // http: require.resolve('rollup-plugin-node-builtins'),
-      // util: require.resolve('rollup-plugin-node-builtins'),
-      // stream: require.resolve('rollup-plugin-node-builtins'),
-      // buffer: require.resolve('rollup-plugin-node-builtins'),
-      // process: require.resolve('rollup-plugin-node-builtins'),
-      // url: require.resolve('rollup-plugin-node-builtins'),
-      // querystring: require.resolve('rollup-plugin-node-builtins'),
     },
   },
   build: {
