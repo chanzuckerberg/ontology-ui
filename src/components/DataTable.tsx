a2346b116eb4a8eb35b3328d14595e83036d3a82
Colin Megill <colinmegill@gmail.com>
2023-04-17 19:38:06 -0700

    data table with csv download

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -4,6 +4,7 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { dataTableIsOpenState } from "../recoil/datatable";
 import { currentCelltypesState, currentCelltypesWithCountsState, currentOntologyState } from "../recoil/";
 import { portalCellTypeCountsState } from "../recoil/portal";
+import { useState } from "react";
 
 const Datatable = () => {
   /* atoms */
@@ -12,9 +13,48 @@ const Datatable = () => {
   const portalCellTypeCounts = useRecoilValue(portalCellTypeCountsState);
   const ontology = useRecoilValue(currentOntologyState);
 
+  // State to keep track of the CSV data
+  const [csvData, setCsvData] = useState("");
+
   if (!ontology) return null;
 
-  const tableWidth = 800;
+  const tableWidth = 1200;
+  const gridWidths = "100px 335px 110px 170px 150px 200px";
+
+  const downloadCsv = () => {
+    // creating an invisible anchor element
+    const anchor = document.createElement("a");
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+
+    // constructing CSV content
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    // adding headers
+    csvContent += "CL Term, Cell Type, Count, Count With Descendants, Immediate Children, Depth\n";
+
+    // adding data rows
+    currentCelltypes.forEach((celltype) => {
+      const vertex = ontology.get(celltype);
+      const rowData = [
+        celltype,
+        vertex?.label,
+        portalCellTypeCounts[celltype]?.unique_cell_count,
+        portalCellTypeCounts[celltype]?.unique_cell_count_with_descendants,
+        vertex?.children.length,
+        vertex?.depth,
+      ];
+      csvContent += rowData.join(",") + "\n";
+    });
+
+    // setting anchor href and initiating download
+    anchor.href = encodeURI(csvContent);
+    anchor.download = "table-data.csv";
+    anchor.click();
+
+    // removing anchor from DOM
+    document.body.removeChild(anchor);
+  };
 
   return (
     <div id="datatable">
@@ -32,53 +72,66 @@ const Datatable = () => {
       >
         <div className={Classes.DRAWER_BODY}>
           <div className={Classes.DIALOG_BODY}>
-            <div style={{ display: "flex" }}>
-              <div>
-                <div style={{ width: tableWidth, display: "flex", justifyContent: "flex-start" }}>
-                  <pre style={{ marginBottom: 20, lineHeight: 1, fontSize: 10, width: 90 }}>CL Term</pre>
-                  <pre style={{ marginBottom: 20, lineHeight: 1, fontSize: 10, width: 335 }}>Cell Type</pre>
-                  <pre style={{ marginBottom: 20, lineHeight: 1, fontSize: 10, width: 200 }}>Count</pre>
-                  <pre style={{ marginBottom: 20, lineHeight: 1, fontSize: 10, width: 200 }}>
-                    Count With Descendants
-                  </pre>
-                </div>
-                <div>
-                  {currentCelltypes.map((celltype, i) => {
-                    console.log(portalCellTypeCounts[celltype]);
-                    const vertex = ontology.get(celltype);
+            <Button onClick={downloadCsv}>Download CSV</Button>
 
-                    return (
-                      <div
-                        style={{
-                          width: tableWidth,
-                          display: "flex",
-                          justifyContent: "flex-start",
-                          alignItems: "left",
-                          background: i % 2 === 0 ? "rgb(240,240,240)" : "none",
-                          fontSize: 10,
-                          lineHeight: 1,
-                        }}
-                      >
-                        <pre style={{ margin: 0, textAlign: "left", width: 90 }} key={celltype}>
-                          {celltype}
-                        </pre>
-                        <pre style={{ margin: 0, textAlign: "left", width: 300, overflow: "hidden" }} key={celltype}>
-                          {vertex && vertex.label}
-                        </pre>
-                        <pre style={{ margin: 0, marginLeft: 35, textAlign: "left", width: 200 }} key={celltype}>
-                          {portalCellTypeCounts[celltype] && portalCellTypeCounts[celltype].unique_cell_count}
-                        </pre>
-                        <pre style={{ margin: 0, textAlign: "left", width: 200 }} key={celltype}>
-                          {portalCellTypeCounts[celltype] &&
-                            portalCellTypeCounts[celltype].unique_cell_count_with_descendants}
-                        </pre>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
+            <div
+              style={{
+                width: tableWidth,
+                display: "grid",
+                gridTemplateColumns: gridWidths,
+                alignItems: "left",
+                marginBottom: 20,
+                fontSize: 18,
+                fontWeight: 900,
+              }}
+            >
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>CL Term</pre>
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>Cell Type</pre>
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>Count</pre>
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>Count With Descendants</pre>
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>Immediate Children</pre>
+              <pre style={{ lineHeight: 1, fontSize: 10 }}>Depth (from ontology root "cell")</pre>
             </div>
-            <div>foo</div>
+            <div>
+              {currentCelltypes.map((celltype, i) => {
+                console.log(portalCellTypeCounts[celltype]);
+                const vertex = ontology.get(celltype);
+
+                return (
+                  <div
+                    style={{
+                      width: tableWidth,
+                      display: "grid",
+                      gridTemplateColumns: gridWidths,
+                      alignItems: "left",
+                      background: i % 2 === 0 ? "rgb(240,240,240)" : "none",
+                      fontSize: 10,
+                      lineHeight: 1,
+                    }}
+                  >
+                    <pre style={{ margin: 0, textAlign: "left" }} key={celltype}>
+                      {celltype}
+                    </pre>
+                    <pre style={{ margin: 0, textAlign: "left", overflow: "hidden", marginRight: 30 }} key={celltype}>
+                      {vertex && vertex.label}
+                    </pre>
+                    <pre style={{ margin: 0, textAlign: "left" }} key={celltype}>
+                      {portalCellTypeCounts[celltype] && portalCellTypeCounts[celltype].unique_cell_count}
+                    </pre>
+                    <pre style={{ margin: 0, textAlign: "left" }} key={celltype}>
+                      {portalCellTypeCounts[celltype] &&
+                        portalCellTypeCounts[celltype].unique_cell_count_with_descendants}
+                    </pre>
+                    <pre style={{ margin: 0, textAlign: "left" }} key={celltype}>
+                      {vertex?.children.length}
+                    </pre>
+                    <pre style={{ margin: 0, textAlign: "left" }} key={celltype}>
+                      {vertex?.depth}
+                    </pre>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </Drawer>
